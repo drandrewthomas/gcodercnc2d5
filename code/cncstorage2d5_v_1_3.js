9e361b1e353b20ee142692b55b91bba3dd6a1890
Andrew Thomas <drandrewthomas@users.noreply.github.com>
2019-08-22 18:44:14 +0100

    Updated to v1.3

---
+++ code/cncstorage2d5_v_1_3.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// ONE VERSION MUST BE COMMENTED OUT !!!
+
+
+// GENERAL WEB APP VERSION USING WINDOW.LOCALSTORAGE
+
+function savesettingslocally()
+{
+  if(!window.localStorage) return;
+  window.localStorage.setItem("grblmode",grblmode);
+  window.localStorage.setItem("safez",safez);
+  window.localStorage.setItem("theme",theme);
+}
+
+function loadsettingslocally()
+{
+  var it;
+  it=window.localStorage.getItem("safez");
+  if(it!==null && !isNaN(it)) safez=parseFloat(it);
+  it=window.localStorage.getItem("grblmode");
+  if(it!==null && !isNaN(it)) grblmode=parseInt(it);
+  it=window.localStorage.getItem("theme");
+  if(it!==null && !isNaN(it)) theme=parseInt(it);
+  //console.log(window.localStorage);
+}
+
+
+// CHROME APP VERSION USING CHROME.STORAGE
+// Use "permissions": [ "storage" ],
+
+/*
+function savesettingslocally()
+{
+  chrome.storage.local.set({"grblmode":grblmode},function(){});
+  chrome.storage.local.set({"safez":safez},function(){});
+  chrome.storage.local.set({"theme":theme},function(){});
+}
+
+function loadsettingslocally()
+{
+  var it;
+  it=window.localStorage.getItem("safez");
+  chrome.storage.local.get(['safez'],function(result){
+    it=result.key;
+    if(it!==null && !isNaN(it)) safez=parseFloat(it);
+  });
+  chrome.storage.local.get(['grblmode'],function(result){
+    it=result.key;
+    if(it!==null && !isNaN(it)) grblmode=parseInt(it);
+  });
+  chrome.storage.local.get(['theme'],function(result){
+    it=result.key;
+    if(it!==null && !isNaN(it)) theme=parseInt(it);
+  });
+}
+
+*/
