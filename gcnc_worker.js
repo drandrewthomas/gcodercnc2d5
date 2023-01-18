6495f5f2d6ea80ea803a65f75397c7b791af63cb
Andrew Thomas <drandrewthomas@users.noreply.github.com>
2023-01-18 13:34:14 +0000

    Add files via upload
    Updating to v1.7.

---
+++ gcnc_worker.js
@@ -0,0 +1,63 @@
+var cacheName = 'gcnc_pwa_v1_7';
+
+var filesToCache =
+[
+  './',
+  './index.html',
+  './code/cncsvgparser_v_1_7.js',
+  './code/gcodercnc_v_1_7.js',
+  './code/gcodercnc_v_1_7.css',
+  './code/cncvariables_v_1_7.js',
+  './code/cncstorage_v_1_7.js',
+  './code/cncgcode_v_1_7.js',
+  './libraries/popper.min.js',
+  './libraries/jquery.min.js',
+  './libraries/bootstrap.min.js',
+  './libraries/bootstrap.min.css',
+  './libraries/FileSaver.min.js',
+  './images/th_humptyegg.png',
+  './images/th_happyface.png',
+  './images/th_flowervcarve.png',
+  './images/th_dragonplaque.png',
+  './images/th_dollshousecrate.png',
+  './images/th_darthvaderhead.png',
+  './images/th_cncshopsign.png'
+];
+
+self.addEventListener('install', function(e)
+{
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache)
+    {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e)
+{
+  e.respondWith(
+    caches.match(e.request).then(function(response)
+    {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e)
+{
+  e.waitUntil(
+    caches.keys().then((keyList) =>
+    {
+      return Promise.all(keyList.map((key) =>
+      {
+        if (key !== cacheName)
+        {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
